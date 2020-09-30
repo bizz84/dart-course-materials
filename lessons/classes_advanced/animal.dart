a4d2012626371825368629b931e92152bfd7b209
Andrea Bizzotto <bizz84@gmail.com>
2020-09-30 12:33:39 +0100

    Add constructors

--- lessons/classes_advanced/animal.dart
+++ lessons/classes_advanced/animal.dart
@@ -1,29 +1,36 @@
 class Animal {
+  const Animal({required this.age});
+  final int age;
+
   void sleep() => print('sleep');
 }
 
 class Dog extends Animal {
+  Dog({required int age}) : super(age: age);
+
   void bark() => print('bark');
 }
 
 class Cow extends Animal {
+  Cow({required int age}) : super(age: age);
   void moo() => print('moo');
 }
 
 class CleverDog extends Dog {
+  CleverDog({required int age}) : super(age: age);
   void catchBall() => print('catch');
 }
 
 void main() {
-  final animal = Animal();
+  final animal = Animal(age: 10);
   animal.sleep();
-  final dog = Dog();
+  final dog = Dog(age: 10);
   dog.bark();
   dog.sleep();
-  final cow = Cow();
+  final cow = Cow(age: 10);
   cow.moo();
   cow.sleep();
-  final cleverDog = CleverDog();
+  final cleverDog = CleverDog(age: 10);
   cleverDog.catchBall();
   cleverDog.bark();
   cleverDog.sleep();
