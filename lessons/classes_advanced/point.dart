8ea8d92284058a1893b133f8c41e66a12da45d16
Andrea Bizzotto <bizz84@gmail.com>
2020-10-01 12:09:58 +0100

    Update Point class with + and * operators

--- lessons/classes_advanced/point.dart
+++ lessons/classes_advanced/point.dart
@@ -10,8 +10,19 @@ class Point {
   bool operator ==(covariant Point other) {
     return x == other.x && y == other.y;
   }
+
+  Point operator +(Point other) {
+    return Point(x + other.x, y + other.y);
+  }
+
+  Point operator *(int other) {
+    return Point(x * other, y * other);
+  }
 }
 
 void main() {
-  print(Point(0, 0) == Point(0, 0));
+  // make this compile by overriding the + operator
+  print(Point(1, 1) + Point(2, 0)); // should print: Point(3, 1)
+  // make this compile by overriding the * operator
+  print(Point(2, 1) * 5); // should print: Point(10, 5)
 }
