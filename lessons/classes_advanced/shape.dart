8d5f9ed98e293eccc6c82e943b72388696cc79b9
Andrea Bizzotto <bizz84@gmail.com>
2020-10-05 17:32:13 +0100

    Add factory constructor to shape class

--- lessons/classes_advanced/shape.dart
+++ lessons/classes_advanced/shape.dart
@@ -2,10 +2,31 @@ import 'dart:math';
 
 abstract class Shape {
   double get area;
+  const Shape();
+
+  factory Shape.fromJson(Map<String, Object> json) {
+    final type = json['type'];
+    switch (type) {
+      case 'square':
+        final side = json['side'];
+        if (side is double) {
+          return Square(side);
+        }
+        throw UnsupportedError('invalid or missing side property');
+      case 'circle':
+        final radius = json['radius'];
+        if (radius is double) {
+          return Circle(radius);
+        }
+        throw UnsupportedError('invalid or missing radius property');
+      default:
+        throw UnimplementedError('shape $type not recognized');
+    }
+  }
 }
 
 class Square extends Shape {
-  Square(this.side);
+  const Square(this.side);
   final double side;
 
   @override
@@ -13,7 +34,7 @@ class Square extends Shape {
 }
 
 class Circle extends Shape {
-  Circle(this.radius);
+  const Circle(this.radius);
   final double radius;
 
   @override
@@ -25,14 +46,19 @@ void printArea(Shape shape) {
 }
 
 void main() {
-  //final shape = Shape();
-  final Shape square = Square(10);
-  printArea(square);
-  final Shape circle = Circle(5);
-  printArea(circle);
-  final shapes = [
-    Square(2),
-    Circle(3),
+  final shapesJson = [
+    {
+      'type': 'square',
+      'side': 10.0,
+    },
+    {
+      'type': 'circle',
+      'radius': 5.0,
+    },
+    {
+      'type': 'triangle',
+    },
   ];
+  final shapes = shapesJson.map((shapeJson) => Shape.fromJson(shapeJson));
   shapes.forEach(printArea);
 }
